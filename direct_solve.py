ac9deaa7c368209dc8a4ef617f9a68c44cdaa09f
Neverov Andrei <line.yka765@gmail.com>
2025-08-26 15:33:41 +0700

    added direct prblem runner

---
+++ direct_solve.py
@@ -0,0 +1,92 @@
+import numpy as np
+import yaml
+import matplotlib.pyplot as plt
+import pandas as pd
+from copy import deepcopy as copy
+from model import Data, ode_objective, ode_solve, euler, eval_grads, precompile_model
+import sciris as sc
+
+with open("tb-mbr/paramteres.yml", "r", encoding="utf8") as file:
+    p = yaml.safe_load(file)
+
+def output(string: str):
+    with open("output.txt", "a") as file:
+        file.write(string + "\n")
+
+
+n=3 #number of various initial states
+
+initial_state = p["initial_state"]
+for key in initial_state.keys():
+    #for i, val in enumerate(initial_state[key]):
+        initial_state[key] = [initial_state[key]]*n
+params = p["default_params"]
+
+for key in params.keys():
+    params[key] = np.array([i/n*params[key] for i in range(1,n+1)])
+print(initial_state)
+t_start = 2009
+modelling_time = 10
+t_end = t_start + modelling_time
+
+step = 1/120
+
+times_to_save = list(range(1,modelling_time+1))
+steps_to_save = np.array(times_to_save)/step
+
+model_kwargs = {
+    "initial_state": initial_state,
+    "t_start": t_start,
+    "t_end": t_end,
+    "step":step,
+    "steps_to_save":steps_to_save,
+} | p["model_kwargs"]
+
+equation_strings, custom_vars = precompile_model(model_kwargs['equation_strings'], model_kwargs['custom_vars'])
+model_kwargs['equation_strings'] = equation_strings
+model_kwargs['custom_vars'] = custom_vars
+
+results = ode_solve(params=params, **model_kwargs)
+
+print(results)
+print(results.data.shape)
+print(results.points.shape)
+
+#------------------------
+#--------Plotting--------
+#------------------------
+
+def plotting(key):
+    c='blue'
+    m=''
+    if 'T' in key:
+        m='+'
+    if 'I' in key:
+        c='red'
+    if 'g' in key:
+        c = 'dark'+c
+    plt.plot(results[key].points, results[key].data, label=key,marker=m,markevery=120,c=c)
+
+
+plotting('S')
+plt.show()
+
+for key in ['EsT','Es', 'IsT', 'Is']:#key = 's'
+    plotting(key)
+plt.legend()
+#plt.yscale('log')
+plt.show()
+
+for key in ['EgT','Eg', 'IgT', 'Ig']:#key = 's'
+    plotting(key)
+plt.legend()
+#plt.yscale('log')
+plt.show()
+
+
+    
+
+print(results[key].points, results[key].data)
+print('solved direct')
+    
+#connection.close()
